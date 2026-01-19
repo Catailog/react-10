d48a4f72d7a504d4896f7f5032d111d259bb46a8
Catailog <tlsdudghks11@gmail.com>
2026-01-19 19:18:40 +0900

    feat: 9강 마운트/언마운트 예시 추가

--- src/lectures/lecture09/Lecture09.tsx
+++ src/lectures/lecture09/Lecture09.tsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 
 import { Button } from '@/components/Button';
 
-export default function Lecture09() {
-  const [count, setCount] = useState<number>(0);
-
-  // deps가 없으면 마운트/언마운트 시에만 실행
+const UnmountTest = forwardRef(() => {
   useEffect(() => {
-    console.log('useEffect(deps: []) 마운트 시 실행');
+    console.log('useEffect(deps: []) UnmountTest 마운트 시 실행');
 
     return () => {
-      console.log('useEffect cleanup(deps: []) 언마운트 시 실행');
+      console.log('useEffect cleanup(deps: []) UnmountTest 언마운트 시 실행');
     };
   }, []);
+
+  return <div>Unmount Test</div>;
+});
+
+export default function Lecture09() {
+  const [count, setCount] = useState<number>(0);
+  const [show, setShow] = useState<boolean>(true);
   // deps가 있으면 deps가 변할 시 실행
   useEffect(() => {
     console.log('useEffect(deps: [count]) count가 변하면 실행');
@@ -22,9 +26,19 @@ export default function Lecture09() {
     };
   }, [count]);
 
-  const onClick = () => {
+  const increaseCount = () => {
     setCount((prev) => prev + 1);
   };
+  const unmountTestRef = useRef<unknown>(null);
+  const toggleUnmountTest = () => {
+    setShow((prev) => !prev);
+  };
 
-  return <Button onClick={onClick}>count: {count}</Button>;
+  return (
+    <>
+      <Button onClick={increaseCount}>count: {count}</Button>
+      <Button onClick={toggleUnmountTest}>div 토글</Button>
+      {show && <UnmountTest ref={unmountTestRef} />}
+    </>
+  );
 }
