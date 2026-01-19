b5ebfa8834af3b8a4b1c6cbce75a6d65e5ebdf87
Catailog <tlsdudghks11@gmail.com>
2026-01-19 19:18:03 +0900

    refactor: 버튼 컴포넌트 props 인터페이스 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,10 @@
-export function Button({ children, onClick }: { children: React.ReactNode; onClick: () => void }) {
+interface ButtonProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+export function Button(buttonProps: ButtonProps) {
+  const { children, onClick } = buttonProps;
   return (
     <button
       className="cursor-pointer rounded-lg bg-emerald-300 px-4 py-2 font-bold hover:bg-emerald-500 active:bg-emerald-700"
